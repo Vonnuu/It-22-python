cabdba41dd9467bac7b955f836ccd0bfc1991c3a
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2022-10-10 19:34:49 +0300

    Add files via upload

---
+++ Kujundid 8.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+aken = turtle.Screen()
+aken.setup(400,400)
+aken.title("ylessanne 3 - KRM")
+
+
+
+k1 = turtle.Turtle()
+for i in range(8):
+    k1.rt(90)
+    k1.fd(20)
+    k1.rt(90)
+    k1.fd(100)
+    k1.rt(90)
+    k1.fd(20)
+    k1.rt(90)
+    k1.fd(100)
+    k1.rt(90)
+    k1.fd(20)
+    k1.lt(135)
