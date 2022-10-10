2ff9248ce1b3a7414520f832dde67f80cfd38f72
Vonnuu <104931173+Vonnuu@users.noreply.github.com>
2022-10-10 13:18:42 +0300

    Add files via upload

---
+++ kilpkonn Ü01.py
@@ -0,0 +1,56 @@
+#Karl Robert Masing
+#10.10.22
+#Harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(400,400)
+
+k1 = turtle.Turtle()
+k2 = turtle.Turtle()
+k3 = turtle.Turtle()
+k4 = turtle.Turtle()
+k5 = turtle.Turtle()
+k6 = turtle.Turtle()
+k7 = turtle.Turtle()
+k8 = turtle.Turtle()
+#teeme kilpkonna
+#tavi = turtle.Turtle()
+#tavi.color("blue")
+#tavi.forward(150)
+#tavi.left(90)
+#tavi.fd(100) #sama mis forward
+#tavi.lt(90) #sama mis left
+#tavi.fd(150)
+#tavi.lt(90)
+#tavi.fd(90)
+k1.color("red")
+k2.color("lime")
+k3.color("orange")
+k4.color("blue")
+k5.color("blue")
+k6.color("lime")
+k7.color("orange")
+k8.color("red")
+
+k1.fd(90)
+k2.left(45)
+k2.fd(90)
+k3.left(90)
+k3.forward(90)
+k4.left(135)
+k4.fd(90)
+k5.lt(180)
+k5.fd(90)
+k6.lt(225)
+k6.fd(90)
+k7.lt(270)
+k7.fd(90)
+k8.lt(315)
+k8.fd(90)
+
+
+
+#suleb akna klikkides
+turtle.exitonclick()
