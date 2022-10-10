467a6a4c67f771b4fdfea388e492f8e661918660
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2022-10-10 14:06:10 +0300

    Add files via upload

---
+++ Karl Robert Masing kujundid 1,9 ja 6.py
@@ -0,0 +1,63 @@
+#Karl Robert Masing
+#10.10.22
+#Harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(400,400)
+aken.title("üleanne 2 - KRM")
+
+
+
+
+
+
+
+
+#1 ja 9 esimese jaoks pead lihtsalt eemaldama 20 ja 21 line
+#k1 = turtle.Turtle()
+#for j in range(4):
+    #k1.rt(90)
+    #for i in range(4):
+        #k1.rt(90)
+        #k1.fd(100)
+        #k1.rt(90)
+        #k1.fd(100)
+        #k1.lt(90)
+        #k1.fd(100)
+        
+        
+        
+        
+        
+        
+#6      
+#k1 = turtle.Turtle()
+#for j in range (8):
+    #k1.rt(90)
+    #k1.fd(20)
+    #k1.rt(90)
+    #k1.fd(100)
+    #k1.rt(90)
+    #k1.fd(20)
+    #k1.rt(90)
+    #k1.fd(100)
+    #k1.rt(90)
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+#aken suleb klikkides
+turtle.exitonclick()
