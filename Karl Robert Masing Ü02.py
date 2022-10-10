1afcf7a09d9d8de5acaacf54b68a5d481b038f0f
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2022-10-10 13:35:42 +0300

    Add files via upload

---
+++ Karl Robert Masing Ü02.py
@@ -0,0 +1,41 @@
+#Karl Robert Masing
+#10.10.22
+#Harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(400,400)
+aken.title("üleanne 2 - KRM")
+
+
+
+
+
+
+
+
+
+k1 = turtle.Turtle()
+for i in range(5):
+    k1.fd(100)
+    k1.lt(144)
+
+
+for i in range(3):
+    k1.color("red")
+    k1.backward(100)
+    k1.rt(120)
+
+
+
+
+
+
+
+
+
+
+
+#aken suleb klikkides
+turtle.exitonclick()
