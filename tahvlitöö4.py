76122383e2489b7b3e817652f659f590c8b5ab6c
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2022-11-14 13:59:49 +0200

    Add files via upload

--- tahvlitöö4.py
+++ tahvlitöö4.py
@@ -1,6 +1,52 @@
-import urllib.request, json
-resp = urllib.request.urlopen('https://query2.finance.yahoo.com/v10/finance/quoteSummary/tsla?modules=price')
-data = json.loads(resp.read())
-price = data['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
-print(price)
-        
+#karl robert masing
+
+#mündid
+def mundid(qw):
+    
+    
+
+
+
+
+#Tervitused motisklustega
+def tervitus(s):
+    for i in range(s):
+        print("Tana",i+1,". kord tervitada, motiskleb voorustaja.")
+        print("Kylaline: \"Tere, suur tanu kutse eest!\"")
+        print("voorustaja: \"Tere!\"")
+ew=int(input("Sisesta mitu korda tahad tervitada: "))
+tervitus(ew)
+
+print("------------------------------------------")
+#Peo eelarve
+def eelarve(kj):
+    eelarve=10*kj+55
+    return eelarve
+y=int(input("osalejate arv: "))
+t=int(input("kinnitatud osalejad: "))
+print(eelarve(y),"€", "maksimaalne")
+print(eelarve(t),"€", "minimaalne")
+
+print("------------------------------------------")
+#Ounamahla tegemine
+def mahlapakk(o):
+    m=round(o*0.4/3)
+    return m
+k=float(input("Sisesta õunad: "))
+print(mahlapakk(k))
+print("------------------------------------------")
+#banner
+def banner(a):
+    a=a.upper()
+    return a
+
+n=input("Sisesta reklaam sona: ")
+m=int(input("Mitu korda soovite reklaami kuvada?: "))
+for i in range(m):
+    print(n.upper())
+
+print("------------------------------------------")
+
+a="Osta ja sa ei kahetse"
+print(a.upper())
+banner(a)
