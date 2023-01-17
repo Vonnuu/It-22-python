478505936f13b2b25c09116b9420e8fd3d3cf313
Karl Robert Masing <104931173+Vonnuu@users.noreply.github.com>
2023-01-17 08:48:09 +0200

    Add files via upload

---
+++ tinker/tkinter ÜL3.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+
+aken = Tk()
+aken.resizable(0,0)
+tekst = Label(aken, text="Nimi: Karl Robert Masing\nRühm: IT22\n2022", foreground="red", background="black", pady=20, padx=30, font="Tahoma 16 bold italic")
+tekst.pack()
+aken.mainloop()
